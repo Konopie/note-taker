0fc17c9b46bb171ce95a6e3c4a368dd83beb4421
Cody Cooper <codycooper06@gmail.com>
2022-08-03 17:21:09 -0400

    api route fixes

--- Develop/server.js
+++ Develop/server.js
@@ -7,11 +7,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.use('/', HTMLroutes);
 app.use('/api', APIroutes);
-
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
